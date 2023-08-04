c1fb9f3ed14517a1092d81684583fefa242bf138
Son Pham on Mac <phamthanhson2k2@gmail.com>
2023-08-04 22:27:20 +0700

    Di chuyển các thẻ script lên trên của file home/view.php

---
+++ home.js
@@ -0,0 +1,121 @@
+/*Kết nối và load data từ DB*/
+// $(document).ready(function(){
+//         $.ajax({
+//         url:"module/home/getcty.php",
+// 		type: "GET",
+// 		success: function(data) {
+// 			var jsonData = JSON.parse(data);
+// 			for (var i = 0; i < jsonData.length; i++) {
+// 				//var kinhdo = jsonData[i].tbl_kinhdo;
+// 				//var vido = jsonData[i].tbl_vido;
+// 				//$("#data").append("<p>Kinh độ: " + kinhdo + " - Vĩ độ: " + vido + "</p>");
+//                 $("#data").append("<p>Cty: " + jsonData[i].cty_ten + " - Logo: " + jsonData[i].cty_logo + "</p>");
+// 			}
+// 		}
+//     });
+// });
+
+/*End Kết nối và load data từ DB*/
+
+/* $(document).ready(function(){
+    $('#cmbCongTy').change(function(){
+        var selectedCty = $(this).val();
+        if(selectedCty != '')
+        {
+            alert(selectedCty);
+        }
+    });
+
+}); */
+
+
+
+
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    { foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' });
+
+    
+//const osm = new GeoSearch.OpenStreetMapProvider();
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("Toạ độ " + e.latlng.toString())
+        .openOn(map);
+}
+
+//map.on('click', onMapClick);
+//Petrolimex
+var ch1 = L.marker([10.04745, 105.77828]).bindTooltip("Cửa Hàng Xăng dầu Petrolimex số 1", { permanent: true, direction: 'right' });
+var ch2 = L.marker([10.02327, 105.76969]).bindTooltip("Cửa Hàng Xăng dầu Petrolimex số 2", { permanent: true, direction: 'right' });
+var ch3 = L.marker([10.059438, 105.766228]).bindTooltip("Cửa Hàng Xăng dầu Petrolimex số 3", { permanent: true, direction: 'right' });
+var ch5 = L.marker([10.0449, 105.78065]).bindTooltip("Cửa Hàng Xăng dầu Petrolimex số 5", { permanent: true, direction: 'right' });
+var ch9 = L.marker([10.04433, 105.73514]).bindTooltip("Cửa Hàng Xăng dầu Petrolimex số 9", { permanent: true, direction: 'right' });
+var ch10 = L.marker([10.01548, 105.75994]).bindTooltip("Cửa Hàng Xăng dầu Petrolimex số 10", { permanent: true, direction: 'right' });
+var ch13 = L.marker([10.01012, 105.75144]).bindTooltip("Cửa Hàng Xăng dầu Petrolimex số 13", { permanent: true, direction: 'right' });
+var ch16 = L.marker([10.03747, 105.75924]).bindTooltip("Cửa Hàng Xăng dầu Petrolimex số 16", { permanent: true, direction: 'right' });
+var ch17 = L.marker([10.01883, 105.76235]).bindTooltip("Cửa Hàng Xăng dầu Petrolimex số 17", { permanent: true, direction: 'right' });
+var cty = L.marker([10.01785, 105.78662]).bindTooltip("Công Ty Dịch Vụ Xăng Dầu Petrolimex Cần Thơ", { permanent: true, direction: 'right' });
+
+var petrolStations = L.layerGroup([ch2, ch1, ch3, ch5, ch9, ch10, ch13, ch16, ch17, cty]);
+
+//PVoil
+var pvoilIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+
+var pvoil2 = L.marker([10.031833926097445, 105.77319616931264], { icon: pvoilIcon }).bindTooltip("Pvoil CHXD SỐ 2 CẦN THƠ", { permanent: true, direction: 'right' });
+var pvoil4 = L.marker([10.109838728448281, 105.61816991534369], { icon: pvoilIcon }).bindTooltip("PVOIL CHXD No. 04", { permanent: true, direction: 'right' });
+var pvoil5 = L.marker([10.242132704602076, 105.54974283068738], { icon: pvoilIcon }).bindTooltip("PVOIL CHXD No. 05", { permanent: true, direction: 'right' });
+var pvoil6 = L.marker([10.033176700229813, 105.46708383068737], { icon: pvoilIcon }).bindTooltip("PVOIL PETROMEKONG CHXD SỐ 6", { permanent: true, direction: 'right' });
+var pvoil80 = L.marker([10.080070312274701, 105.74429508465633], { icon: pvoilIcon }).bindTooltip("PVOIL PETROMEKONG CHXD SỐ 80", { permanent: true, direction: 'right' });
+var pvoil81 = L.marker([10.020481672185621, 105.7817321169473], { icon: pvoilIcon }).bindTooltip("PVOIL PETROMEKONG CHXD SỐ 81", { permanent: true, direction: 'right' });
+
+var pvoilStations = L.layerGroup([pvoil2, pvoil4, pvoil5, pvoil6, pvoil80, pvoil81]);
+
+var baseMaps = {
+    "Bản đồ nền": osm
+};
+
+var overlayMaps = {
+    "Petrolimex": petrolStations,
+    "PvOil": pvoilStations
+};
+/*
+var container = L.DomUtil.get('map');
+if (container != null) {
+    container._leaflet_id = null;
+}
+*/
+if (map !== undefined && map !== null) { map.off(); map.remove(); }
+
+//map.off();
+//map.remove();
+var map = L.map('map', { center: [10.0338, 105.7867], zoom: 11.5, layers: [osm, petrolStations, pvoilStations] });
+
+var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+// Creating scale control
+var scale = L.control.scale().addTo(map);
+
+//L.Control.geocoder().addTo(map);
+
+var geocodeService = L.esri.Geocoding.geocodeService();
+
+  map.on('click', function (e) {
+    geocodeService.reverse().latlng(e.latlng).run(
+        function (error, result) {
+            if (error) {
+                return;
+            }
+            L.marker(result.latlng).addTo(map).bindPopup(result.address.Match_addr).openPopup();
+        });
+    });
+
+   var searchControl = L.esri.Geocoding.geosearch({position: "topright"}).addTo(map);
--- module/home/view.php
+++ module/home/view.php
@@ -19,7 +19,27 @@ class HomeView
 			<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
      		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw-src.css" integrity="sha512-vJfMKRRm4c4UupyPwGUZI8U651mSzbmmPgR3sdE3LcwBPsdGeARvUM5EcSTg34DK8YIRiIo+oJwNfZPMKEQyug==" crossorigin="anonymous" />
 			<link rel="stylesheet" href="https://unpkg.com/leaflet-geosearch@3.0.0/dist/geosearch.css" />
-			
+			<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+				integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+				crossorigin=""></script>
+			<script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
+			<link rel="stylesheet" href="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css" />
+			<script src="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js"></script>
+
+			<!-- Load Esri Leaflet from CDN -->
+			<script src="https://unpkg.com/esri-leaflet@2.4.1/dist/esri-leaflet.js"
+				integrity="sha512-xY2smLIHKirD03vHKDJ2u4pqeHA7OQZZ27EjtqmuhDguxiUvdsOuXMwkg16PQrm9cgTmXtoxA6kwr8KBy3cdcw=="
+				crossorigin=""></script>
+
+			<!-- Load Esri Leaflet Geocoder from CDN -->
+			<link rel="stylesheet" href="https://unpkg.com/esri-leaflet-geocoder@2.3.3/dist/esri-leaflet-geocoder.css"
+				integrity="sha512-IM3Hs+feyi40yZhDH6kV8vQMg4Fh20s9OzInIIAc4nx7aMYMfo+IenRUekoYsHZqGkREUgx0VvlEsgm7nCDW9g=="
+				crossorigin="">
+			<script src="https://unpkg.com/esri-leaflet-geocoder@2.3.3/dist/esri-leaflet-geocoder.js"
+				integrity="sha512-HrFUyCEtIpxZloTgEKKMq4RFYhxjJkCiF5sDxuAokklOeZ68U2NPfh4MFtyIVWlsKtVbK5GD2/JzFyAfvT5ejA=="
+				crossorigin=""></script>
+			<script src="https://unpkg.com/leaflet-geosearch@3.0.0/dist/geosearch.umd.js"></script>
+
 				
 			<div class="panel panel-content-cont container">
 				<div class="header-content-cont">
@@ -42,26 +62,7 @@ class HomeView
 				</div>
 			</div>
 
-			<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-        		integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-        		crossorigin=""></script>
-    		<script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
-			<link rel="stylesheet" href="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css" />
-			<script src="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js"></script>
-
-			<!-- Load Esri Leaflet from CDN -->
-			<script src="https://unpkg.com/esri-leaflet@2.4.1/dist/esri-leaflet.js"
-				integrity="sha512-xY2smLIHKirD03vHKDJ2u4pqeHA7OQZZ27EjtqmuhDguxiUvdsOuXMwkg16PQrm9cgTmXtoxA6kwr8KBy3cdcw=="
-				crossorigin=""></script>
-
-			<!-- Load Esri Leaflet Geocoder from CDN -->
-			<link rel="stylesheet" href="https://unpkg.com/esri-leaflet-geocoder@2.3.3/dist/esri-leaflet-geocoder.css"
-				integrity="sha512-IM3Hs+feyi40yZhDH6kV8vQMg4Fh20s9OzInIIAc4nx7aMYMfo+IenRUekoYsHZqGkREUgx0VvlEsgm7nCDW9g=="
-				crossorigin="">
-			<script src="https://unpkg.com/esri-leaflet-geocoder@2.3.3/dist/esri-leaflet-geocoder.js"
-				integrity="sha512-HrFUyCEtIpxZloTgEKKMq4RFYhxjJkCiF5sDxuAokklOeZ68U2NPfh4MFtyIVWlsKtVbK5GD2/JzFyAfvT5ejA=="
-				crossorigin=""></script>
-			<script src="https://unpkg.com/leaflet-geosearch@3.0.0/dist/geosearch.umd.js"></script>
+			
 			';
 		return $html;
 	}
